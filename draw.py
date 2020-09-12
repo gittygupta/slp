73dba8d9154b0394f2cb10df744857e6c48b2a46
Aousnik Gupta <guptaaousnik37@gmail.com>
2020-09-12 20:07:26 +0530

    pose drawing added

--- draw.py
+++ draw.py
@@ -1,4 +1,4 @@
-import tensrflow as tf
+import tensorflow as tf
 import numpy
 import cv2
 
@@ -47,14 +47,18 @@ HAND_PAIRS = [[0, 1], [1, 2], [2, 3], [3, 4],
 POSE_PAIRS = [[0, 1], [1, 2], [2, 3], [3, 4], [1, 5], [5, 6], [6, 7], [1, 8]]
 
 def draw(pred, size, coor='3D'):
+    VEC = tf.convert_to_tensor([size[0] / 2., size[1] / 2.])
+    ADD = tf.convert_to_tensor([size[0] / 2., size[1] / 2.])
+
     pred = pred[:, :-1]
-    if coor == '3D'
-        pred = tf.reshape(pred, (pred.shape[1], -1, 3))
+    if coor == '3D':
+        pred = tf.reshape(pred, (pred.shape[0], -1, 3))
     else:
-        pred = tf.reshape(pred, (pred.shape[1], -1, 2))
-    pose = tf.cast(pred[:, :9, :], dtype=tf.int32)
-    left_hand = tf.cast(pred[:, 9:30, :], dtype=tf.int32)
-    right_hand = tf.cast(pred[:, 30:, :], dtype=tf.int32)
+        pred = tf.reshape(pred, (pred.shape[0], -1, 2))
+    
+    pose = pred[:, :9, :]
+    left_hand = pred[:, 9:30, :]
+    right_hand = pred[:, 30:, :]
 
     if coor == '3D':
         pose = pose[:, :, :-1]
@@ -62,32 +66,68 @@ def draw(pred, size, coor='3D'):
         right_hand = right_hand[:, :, :-1]
 
     for i in range(pred.shape[0]):
-        white = tf.zeros((size[0], size[1], 3), tf.int32)
+        frame = np.zeros((size[0], size[1], 3), np.uint8)
         for pair in POSE_PAIRS:
             partFrom = pair[0]
             partTo = pair[1]
-            cv2.line(white, pose[i][partFrom], pose[i][partTo], (255, 74, 0), 3)
-            cv2.ellipse(white, pose[i][partFrom], (4, 4), 0, 0, 360, (255, 255, 255), cv2.FILLED)
-            cv2.ellipse(white, pose[i][partTo], (4, 4), 0, 0, 360, (255, 255, 255), cv2.FILLED)
+
+            pose_partFrom = tuple(pose[i][partFrom] * VEC + ADD)
+            pose_partTo = tuple(pose[i][partTo] * VEC + ADD)
+
+            if pose_partFrom == (0., 0.) or pose_partTo == (0., 0.):
+                continue
+
+            cv2.line(frame, pose_partFrom, pose_partTo, (255, 74, 0), 1)
+            cv2.ellipse(frame, pose_partFrom, (2, 2), 0, 0, 360, (255, 255, 255), cv2.FILLED)
+            cv2.ellipse(frame, pose_partTo, (2, 2), 0, 0, 360, (255, 255, 255), cv2.FILLED)
             #cv2.putText(white, str(idFrom), points[idFrom], cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255),2,cv2.LINE_AA)
             #cv2.putText(white, str(idTo), points[idTo], cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255),2,cv2.LINE_AA)
 
+        # left
         for pair in HAND_PAIRS:
             partFrom = pair[0]
             partTo = pair[1]
 
-            # left
-            cv2.line(white, left_hand[i][partFrom], left_hand[i][partTo], (255, 74, 0), 3)
-            cv2.ellipse(white, left_hand[i][partFrom], (4, 4), 0, 0, 360, (255, 255, 255), cv2.FILLED)
-            cv2.ellipse(white, left_hand[i][partTo], (4, 4), 0, 0, 360, (255, 255, 255), cv2.FILLED)
+            left_hand_partFrom = tuple(left_hand[i][partFrom] * VEC + ADD)
+            left_hand_partTo = tuple(left_hand[i][partTo] * VEC + ADD)
+
+            if left_hand_partFrom == (0., 0.) or left_hand_partTo == (0., 0.):
+                continue
+
+            cv2.line(frame, left_hand_partFrom, left_hand_partTo, (0, 255, 0), 1)
+            cv2.ellipse(frame, left_hand_partFrom, (2, 2), 0, 0, 360, (255, 255, 0), cv2.FILLED)
+            cv2.ellipse(frame, left_hand_partTo, (2, 2), 0, 0, 360, (255, 255, 0), cv2.FILLED)
             #cv2.putText(white, str(idFrom), points[idFrom], cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255),2,cv2.LINE_AA)
             #cv2.putText(white, str(idTo), points[idTo], cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255),2,cv2.LINE_AA)
 
-            # right
-            cv2.line(white, right_hand[i][partFrom], right_hand[i][partTo], (255, 74, 0), 3)
-            cv2.ellipse(white, right_hand[i][partFrom], (4, 4), 0, 0, 360, (255, 255, 255), cv2.FILLED)
-            cv2.ellipse(white, right_hand[i][partTo], (4, 4), 0, 0, 360, (255, 255, 255), cv2.FILLED)
+        # right
+        for pair in HAND_PAIRS:
+            partFrom = pair[0]
+            partTo = pair[1]
+
+            right_hand_partFrom = tuple(right_hand[i][partFrom] * VEC + ADD)
+            right_hand_partTo = tuple(right_hand[i][partTo] * VEC + ADD)
+
+            if right_hand_partFrom == (0., 0.) or right_hand_partTo == (0., 0.):
+                continue
+
+            cv2.line(frame, right_hand_partFrom,right_hand_partTo, (0, 255, 0), 1)
+            cv2.ellipse(frame, right_hand_partFrom, (2, 2), 0, 0, 360, (255, 255, 0), cv2.FILLED)
+            cv2.ellipse(frame, right_hand_partTo, (2, 2), 0, 0, 360, (255, 255, 0), cv2.FILLED)
             #cv2.putText(white, str(idFrom), points[idFrom], cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255),2,cv2.LINE_AA)
             #cv2.putText(white, str(idTo), points[idTo], cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255),2,cv2.LINE_AA)
 
-        cv2.imwrite(str(i) + '.jpg', white)
+        cv2.imwrite(str(i) + '.jpg', frame)
+
+
+def main():
+    sign_path = 'train'
+    net_seq_len = 512
+    pred = get_processed_data_2d(sign_path, [['02September_2010_Thursday_tagesschau-8371']], 0, net_seq_len)
+    pred = pred[0, :50]
+    draw(pred, size=(300, 300), coor='2D')
+
+if __name__ == '__main__':
+    from data_utils import *
+    main()
+    #cv2.imwrite('hello.jpg', np.zeros((120, 120), dtype=np.uint32) + 255)
